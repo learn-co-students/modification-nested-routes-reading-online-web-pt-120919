c062bd28db23838116759d4c5d4b36bef36006bf
Simon <seunglives@hotmail.com>
2020-06-22 18:28:02 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    resources :authors, only: [:show, :index] do 
-        resources :posts, only: [:show, :index, :new, :edit]
-    end 
-    resources :posts
+   namespace :admin do 
+    resources :stats, only: [:index]
+  end
+    
 end
